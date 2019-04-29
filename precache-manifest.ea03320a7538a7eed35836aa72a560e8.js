5e5de42c07e4a3a083512b387d6a203419ae65c2
Elias <felix.elias@accenture.com>
2019-04-29 14:06:45 -0400

    Updates

---
+++ precache-manifest.ea03320a7538a7eed35836aa72a560e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fd0fdf92e6a480c11b51b4c367d1c80",
+    "url": "/felixchess5/react-playground.git/index.html"
+  },
+  {
+    "revision": "c4b908359f677c1055ac",
+    "url": "/felixchess5/react-playground.git/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/felixchess5/react-playground.git/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "c4b908359f677c1055ac",
+    "url": "/felixchess5/react-playground.git/static/js/main.8c0ec5ec.chunk.js"
+  },
+  {
+    "revision": "92f770b030a4f5509bf9",
+    "url": "/felixchess5/react-playground.git/static/js/runtime~main.d7a11bf9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/felixchess5/react-playground.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
